d285f4c56f62e86dc6d77da2e4486e42299221e7
Michael Li <dev.mike011235@gmail.com>
2020-10-09 19:03:46 -0400

    Created a create user page. Added to routes in App.js for testing. Renders succesfully

--- client/src/pages/CreateAccount.js
+++ client/src/pages/CreateAccount.js
@@ -4,14 +4,24 @@ import { Form, Button } from "react-bootstrap"
 function CreateAccount() {
   return (
     <Form>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="first-name" placeholder="First Name" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="last-name" placeholder="Last Name" />
+      </Form.Group>
+
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Email" />
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="username" placeholder="username" />
       </Form.Group>
 
       <Form.Group controlId="formBasicPassword">
@@ -19,7 +29,11 @@ function CreateAccount() {
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
 
-
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      
       <Button variant="primary" type="submit">
         CreateAccount
       </Button>
