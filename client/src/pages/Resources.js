b17f7c7d8bd2f598fb9bd40658d5663b4badfbd9
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-10-12 03:26:46 -0400

    added async function for making youtube search and added youtube api

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -6,4 +6,17 @@ function Resources() {
   );
 }
 
+async function searchYouTube(q) {
+  q = encodeURIComponent(q);
+  const response = await fetch("https://youtube-search-results.p.rapidapi.com/youtube-search/?q=" + q, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "youtube-search-results.p.rapidapi.com",
+      "x-rapidapi-key": "d365e9ff5bmsh0dfce4ef286ff2bp172503jsne4af471ba465"
+    }
+  });
+  const body = await response.json();
+  console.log(body);
+  return body.items.filter(item => item.type === 'video');
+}
 export default Resources;
