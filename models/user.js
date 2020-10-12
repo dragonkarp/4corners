ca00bc0b31a784791bc885482bffb2bb31b48ce7
Michael Li <dev.mike011235@gmail.com>
2020-10-11 22:49:54 -0400

    Modified user schema to include password validation, handled by bcrypt library. Build runs succesffuly on this commit.

--- models/user.js
+++ models/user.js
@@ -9,15 +9,45 @@ const UserSchema = new Schema({
       unique: true
   },
   password: {
-      type :String
-  }
+      type :String,
+      min: 8,
+      max: 15,
+      required: true
+  },
 });
 
-UserSchema.path('email').validate(function (email) {
-    var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    return emailRegex.test(email.text); // Assuming email has a text attribute
- }, 'The e-mail field cannot be empty.')
+UserSchema.pre("save", function(next) {
+    if (!this.isModified('password'))
+        return next();
+    bcrypt.hash(this.password, 10, (err, passwordHash) => {
+        if (err)
+            return next(err);
+        this.password = passwordHash;
+        next();
+    })
+})
+
+UserSchema.method.comparePassword = function(password, cb) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (err)
+            return cb(err);
+        else {
+            if (!isMatch)
+                return cb(null, isMatch); // Returns error is password does not match.
+            return cb(null, this)
+        }
+    })
+}
+
 
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
+
+
+
+
+// UserSchema.path('email').validate(function (email) {
+//     var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+//     return emailRegex.test(email.text); // Assuming email has a text attribute
+//  }, 'The e-mail field cannot be empty.')
