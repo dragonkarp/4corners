ba157e1b14023b7096a83b62a816ab018598cd68
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-10-12 03:32:31 -0400

    resources function to Store the query string and the list of YouTube videos in-state using React Hooks

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -1,10 +1,51 @@
 import React from "react";
 
+
+//resources function Store the query string and the list of YouTube videos in-state using React Hooks. ----Create a search() function that calls our searchYouTube function.-----Set the result of our YouTube search to the new list variable using our setList setter.---After the search has finished, show it in the view, displaying only the interesting properties
+
+
 function Resources() {
+  const [query, setQuery] = React.useState('European history');
+  const [list, setList] = React.useState(null);
+  const search = (e) => {
+    e.preventDefault();
+    searchYouTube(query).then(setList);
+  };
   return (
-    <h1>Helpful links</h1>
+    <div className="app">
+      <form onSubmit={search}>
+        <input autoFocus value={query} onChange={e => setQuery(e.target.value)} />
+        <button>Search YouTube</button>
+      </form>
+      {list &&
+        (list.length === 0
+          ? <p>No results</p>
+          : (
+            <ul className="items">
+              {list.map(item => (
+                <li className="item" key={item.id}>
+                  <div>
+                    <b><a href={item.link}>{item.title}</a></b>
+                    <p>{item.description}</p>
+                  </div>
+                  <ul className="meta">
+                    <li>By: <a href={item.author.ref}>{item.author.name}</a></li>
+                    <li>Views: {item.views}</li>
+                    <li>Duration: {item.duration}</li>
+                    <li>Uploaded: {item.uploaded_at}</li>
+                  </ul>
+                  <img alt="" src={item.thumbnail} />
+                </li>
+              ))}
+            </ul>
+          )
+        )
+      }
+    </div>
   );
 }
+
+// search youtube function :1. Takes the query string as a parameter, URI-encodes it, and appends it to the API’s URL. 2.Uses await syntax to get the response and get JSON from out of the response’s body. 3. Returns the items key, filtering out everything except items with a type key of "video".
 
 async function searchYouTube(q) {
   q = encodeURIComponent(q);
