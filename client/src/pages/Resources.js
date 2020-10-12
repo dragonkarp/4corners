c8e3a7afd21e60185db66418505389c8af069bca
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-10-12 03:55:57 -0400

    clean up for resourses page

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -28,12 +28,7 @@ function Resources() {
                     <b><a href={item.link}>{item.title}</a></b>
                     <p>{item.description}</p>
                   </div>
-                  <ul className="meta">
-                    <li>By: <a href={item.author.ref}>{item.author.name}</a></li>
-                    <li>Views: {item.views}</li>
-                    <li>Duration: {item.duration}</li>
-                    <li>Uploaded: {item.uploaded_at}</li>
-                  </ul>
+
                   <img alt="" src={item.thumbnail} />
                 </li>
               ))}
