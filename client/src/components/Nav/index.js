81b12a57eecb121f41f50960069ad117d19acde9
Margaret Seiche <margaretseiche@hotmail.com>
2020-10-19 19:47:42 -0400

    added favicon + styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./style.css";
+import logo from "../Nav/4corners_white.png";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark">
+      <img className="navLogo" src={logo} alt="Logo"/>
       <Link to="/Person" className="navbar-brand">
         React Task List
       </Link>
